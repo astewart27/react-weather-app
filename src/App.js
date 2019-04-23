6cdf993d75a65925e8f574c06c9483838c953ea9
Aaron Stewart <astewart@r2integrated.com>
2019-04-22 20:41:35 -0400

    React Weather App initial commit

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import './App.css';
+import Title from './components/Title';
+import Form from './components/Form';
+import Weather from './components/Weather';
+
+const API_KEY = "9a05ae50eeea727c7194e9d7f8dc4a6a";
+
+class App extends Component {
+  
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    condition: undefined,
+    error: undefined
+  };
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`);
+    const data = await api_call.json();
+    if (city && country){
+      this.setState({ 
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidty: data.main.humidty,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({ 
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidty: undefined,
+        description: undefined,
+        error: "Please enter a city and country."
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="title-container">
+                  <Title />
+                </div>
+                <div className="form-container">
+                  <Form getWeather={this.getWeather}/>
+                  <Weather 
+                    temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    description={this.state.description}
+                    error={this.state.error}/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> 
+    );
+  } 
+}
+
+export default App;
---
+++ src/components/WeatherItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class WeatherItem extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default WeatherItem;
